42abaf55c948c4bce36d979a035ea0ccb7c24bb7
MasatoKato <kanperi808@gmail.com>
2024-03-06 16:54:49 +0000

    [Add] devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
+  
+  #会員用
+  devise_for :members, skip: [:passwords], controllers: 
+  {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers:
+  {
+    sessions: "admin/sessions"
+  }
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
