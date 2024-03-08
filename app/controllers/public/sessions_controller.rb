fefdd15e35cb6916c99a918216863eb6cf5c1be4
MasatoKato <kanperi808@gmail.com>
2024-03-08 07:27:02 +0000

    [Add] deviseのフォルダを適切な位置に移動、パスの修正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -24,4 +24,10 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+  
+  def guest_sign_in
+    member = Member.guest
+    sign_in member
+    redirect_to member_path(member), notice: "ゲストでログインしました"
+  end
 end
