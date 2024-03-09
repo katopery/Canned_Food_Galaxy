808cd54d0a0e6062430cbed3fc344c594cab74a8
MasatoKato <kanperi808@gmail.com>
2024-03-09 08:33:17 +0000

    [Add] active storage導入

--- app/models/canned_food.rb
+++ app/models/canned_food.rb
@@ -3,4 +3,6 @@ class CannedFood < ApplicationRecord
   has_many :favorites, dependent: :destroy    # お気に入りとの関連付け
   has_many :canned_tags, dependent: :destroy  # 缶詰タグとの関連付け
   
+  has_one_attached :image  # 缶詰の画像用
+  
 end
