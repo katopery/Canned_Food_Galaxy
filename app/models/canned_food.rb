77bc797917a104c68d5945d0efaa893d0087e5a4
MasatoKato <kanperi808@gmail.com>
2024-03-10 16:58:48 +0000

    [Add] 缶詰新規登録画面作成

--- app/models/canned_food.rb
+++ app/models/canned_food.rb
@@ -2,7 +2,12 @@ class CannedFood < ApplicationRecord
   has_many :reviews, dependent: :destroy      # レビューとの関連付け
   has_many :favorites, dependent: :destroy    # お気に入りとの関連付け
   has_many :canned_tags, dependent: :destroy  # 缶詰タグとの関連付け
+  has_many :tags, through: :canned_tags       # タグとの関連付け
   
   has_one_attached :image  # 缶詰の画像用
   
+  validates :canned_name, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+  validates :is_canned_status, inclusion: { in: [true, false] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,28 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# 管理者ログイン用
+ 管理者ログイン用
 Admin.create!(
    email: 'admin@admin',
    password: 'testtest'
 )
+
+Tag.create([
+    { canned_tag_id: 1, name: 'フルーツ' },
+    { canned_tag_id: 2, name: 'やきとり・とり' },
+    { canned_tag_id: 3, name: '肉類' },
+    { canned_tag_id: 4, name: 'ジビエ・馬肉' },
+    { canned_tag_id: 5, name: 'オイルサーディン' },
+    { canned_tag_id: 6, name: 'サバ' },
+    { canned_tag_id: 7, name: 'サンマ' },
+    { canned_tag_id: 8, name: 'ツナ/マグロ' },
+    { canned_tag_id: 9, name: '貝類' },
+    { canned_tag_id: 10, name: 'その他魚介類' },
+    { canned_tag_id: 11, name: 'カレー' },
+    { canned_tag_id: 12, name: '米類' },
+    { canned_tag_id: 13, name: 'おそうざい系' },
+    { canned_tag_id: 14, name: '漬物・サラダ' },
+    { canned_tag_id: 15, name: 'スープ・ソース' },
+    { canned_tag_id: 16, name: 'お菓子・スナック類' },
+    { canned_tag_id: 17, name: 'その他' }
+    ])
