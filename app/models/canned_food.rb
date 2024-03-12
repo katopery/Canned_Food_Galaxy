87bb741ba87d7f346de85dcb346322096fecaa83
MasatoKato <kanperi808@gmail.com>
2024-03-12 06:48:02 +0000

    [Add] 修正漏れ

--- app/models/canned_food.rb
+++ app/models/canned_food.rb
@@ -3,25 +3,18 @@ class CannedFood < ApplicationRecord
   has_many :favorites, dependent: :destroy    # お気に入りとの関連付け
   has_many :canned_tags, dependent: :destroy  # 缶詰タグとの関連付け
   has_many :tags, through: :canned_tags       # タグとの関連付け
-  
+
   has_one_attached :image  # 缶詰の画像用
-  
-  def get_image(width, height) 
+
+  def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
       image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
-    end 
-    
+    end
+
     image.variant(resize_to_limit: [width,height]).processed
   end
-  
-  #部分一致検索
-  def self.looks(search, word)
-    if search == "partial"
-      @canned_foods = CannedFood.where("canned_name LIKE?","%#{word}%")
-    end
-  end
-  
+
   validates :canned_name, presence: true
   validates :description, presence: true
   validates :image, presence: true
