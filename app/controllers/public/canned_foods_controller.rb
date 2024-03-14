bc3c17e4fe65beda12a5ea976145b104ee1e18de
MasatoKato <kanperi808@gmail.com>
2024-03-14 15:56:26 +0000

    [Add] top画面の作成

--- app/controllers/public/canned_foods_controller.rb
+++ app/controllers/public/canned_foods_controller.rb
@@ -8,10 +8,6 @@ class Public::CannedFoodsController < ApplicationController
   def show
     @canned_food = CannedFood.find(params[:id])
     @tags = @canned_food.tags
-    @member = Member.find(current_member.id)
-    
-    
-    
     
     # 各レビューの平均値算出
     @expiry_date_avg = @canned_food.reviews.average(:expiry_date_rating) || 0
@@ -19,13 +15,19 @@ class Public::CannedFoodsController < ApplicationController
     @snack_avg = @canned_food.reviews.average(:snack_rating) || 0
     @outdoor_avg = @canned_food.reviews.average(:outdoor_rating) || 0
     
-    # すでに評価済みかの確認フラグ
-    @review = Review.find_by(member_id: current_member.id, canned_food_id: params[:id])
-    
-    # 新規レビュー作成用
+
+    if current_member
+      # 会員がログインしている場合
+      @member = Member.find(current_member.id)
+      @review = Review.find_by(member_id: current_member.id, canned_food_id: params[:id])
+    else
+      # 会員がログインしていない場合
+      @member = nil
+    end
+
     if @review == nil
+       # 新規レビュー作成用
       @review = @canned_food.reviews.build
     end
-    
   end
 end
