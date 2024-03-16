6b57f7394354edd54fafaab12592826fdb60bb85
MasatoKato <kanperi808@gmail.com>
2024-03-16 08:21:30 +0000

    [Add] コメント機能

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,25 +3,36 @@ class Public::CommentsController < ApplicationController
     review = Review.find(params[:review_id])
     comment = current_member.comments.new(comment_params)
     comment.review_id = review.id
-    comment.save
-    redirect_to review_comments_path(review)
+    
+    if comment.save
+      # コメントの保存が成功した場合の処理
+      redirect_to request.referer
+    else
+      # コメントの保存が失敗した場合の処理
+      flash[:error] = comment.errors.full_messages.join(", ")
+      redirect_to request.referer
+    end
   end
 
   def index
     @review = Review.find(params[:review_id])
     @canned_food = @review.canned_food
-    @reviewer = @review.member
+    @tags = @canned_food.tags
     
     @comment = Comment.new
-    @commenter = Member.new
+    
+    @comments = @review.comments.page(params[:page]).per(5)
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to request.referer
   end
   
   private
 
   def comment_params
-     params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:content, :image)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :member  # 会員に所属
   belongs_to :review  # レビューに所属
   
-  validates :comment, presence: true
+  has_one_attached :image
+  def get_image(width, height)
+    if image.attached?
+      image.variant(resize_to_limit: [width,height]).processed
+    end
+  end
+  
+  validates :content, presence: true
 end
